4a5704a84f2078f7a2357eec35f2113e32060d57
natrix_dev <discordlenko@gmail.com>
2022-02-16 18:22:28 +0100

    Create insta_bot.py

---
+++ insta_bot.py
@@ -0,0 +1,37 @@
+from pypage import *
+import natrix-insta.py as .
+
+# connfig 
+name = "instagram username (@natrix)"
+pass = "password"
+hash = "#you want to sub"
+
+random = true # dont change this
+bOt_not_dect = true # dont chnage this
+
+
+
+pypage.newwindow(script=>{
+  def _ for x in range(random(1;999):
+  .init & .login && .sub && .dislogin
+  
+  else: 
+    destroy() => print("bot was detected, program stopped")
+    
+    
+    .checkIf(Account.Detected) => if yes == yes :
+    destroy & print("stopped, account found by intagram anti-bot")
+    else => run()
+
+
+
+
+
+
+
+
+
+
+
+
+}
